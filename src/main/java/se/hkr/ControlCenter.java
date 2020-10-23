578001e8804273b85ad4573137a55bae45cb1e5d
Nemanja Negovanovic <nem.neg@hotmail.com>
2020-10-23 18:23:18 +0200

    implement initial logic
    Initial logic for the smart house mock implemented
    ControlCenter.java - Added initial logic
    SmartHouse.java - Added initial logic
    .gitignore updated

--- src/main/java/se/hkr/ControlCenter.java
+++ src/main/java/se/hkr/ControlCenter.java
@@ -1,8 +1,7 @@
 package se.hkr;
 
-import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import se.hkr.models.House;
 
 import java.util.ArrayList;
@@ -23,39 +22,76 @@ public class ControlCenter {
     private static MqttClient client;
     private static String prefix = "smart_house/";
 
-
-    private House house;
+    SmartHouse smartHouse = SmartHouse.getInstance();
+    private House house = smartHouse.getHouse();
 
 
     public static void main(String[] args) {
         controlCenter = ControlCenter.getInstance();
 
         controlCenter.connectMqtt();
-        controlCenter.startAlarm();
-        controlCenter.togglePausePlayAlarm();
+        controlCenter.smartHouse.houseMock();
+        controlCenter.smartHouse.outputsMockData();
+//        controlCenter.startAlarm();
+//        controlCenter.togglePausePlayAlarm();
     }
 
     /**
      * startAlarm() should be used when a user wants to initialize the
      * alarm sound playing class
      */
-    public void startAlarm(){}
+    public void startAlarm() {
+    }
 
     /**
      * togglePausePlayAlarm() toggles between play/pause functions in
      * sound player class
      */
-    public void togglePausePlayAlarm(){}
+    public void togglePausePlayAlarm() {
+    }
 
     /**
      * subscribeToTopics() subscribes to all necessary MQTT topics
      */
-    public void subscribeToTopics(){}
+    public void subscribeToTopics(MqttClient client) throws MqttException {
+        for (String s: topics) {
+            String topic = prefix + s;
+            client.subscribe(topic, new MqttMessageListener());
+            System.out.println("subscribed to: " + topic);
+        }
+    }
 
     /**
      * connectMqtt() connects to MQTT broker
      */
-    public void connectMqtt(){}
+    public void connectMqtt() {
+
+        new Thread(() -> {
+            //Setting up the connection
+            String port = "1883";
+            String broker = "tcp://mqtthive.ddns.net" + ":" + port;
+            MemoryPersistence persistence = new MemoryPersistence();
+
+            try {
+                client = new MqttClient(broker, "House_mock", persistence);
+
+                //Creating the options
+                MqttConnectOptions connOpts = new MqttConnectOptions();
+                connOpts.setCleanSession(true);
+                connOpts.setKeepAliveInterval(180);
+                client.connect(connOpts);
+                System.out.println("broker: " + broker + " Connected");
+                subscribeToTopics(client);
+                smartHouse.setClient(client);
+                //client.publish("smart_house/indoor_temperature", new M);
+
+
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+    }
 
     /**
      * getInstance() singleton method
@@ -63,10 +99,19 @@ public class ControlCenter {
      * @return ControlCenter instance
      */
     public static ControlCenter getInstance() {
-        if(controlCenter == null){
+        if (controlCenter == null) {
             controlCenter = new ControlCenter();
         }
         return controlCenter;
+    }
+
+    /**
+     * getHouse()
+     * @return house
+     */
+
+    public House getHouse(){
+        return house;
     }
 
     /**
@@ -74,12 +119,56 @@ public class ControlCenter {
      * before publishing to broker
      * this method is used for error handling
      */
-    public void clientCheck(){}
+    public void clientCheck() {
+        System.out.println("Checking MQTT connection ...");
+        if (!client.isConnected()){
+            System.out.println("Reconnecting ...");
+            connectMqtt();
+        } else {
+            System.out.println("Connection to MQTT is stable.");
+        }
+    }
 
 }
+
 class MqttMessageListener implements IMqttMessageListener {
     @Override
     public void messageArrived(String var1, MqttMessage var2) throws Exception {
-
+        ControlCenter controlCenter = ControlCenter.getInstance();
+        switch (var1){
+            case "smart_house/indoor_light":
+                controlCenter.getHouse().setIndoorLight(Boolean.parseBoolean(var2.toString()));
+                break;
+            case "smart_house/outdoor_light":
+                controlCenter.getHouse().setOutdoorLight(Boolean.parseBoolean(var2.toString()));
+                break;
+            case "smart_house/alarm":
+                controlCenter.getHouse().setAlarmArmed(Boolean.parseBoolean(var2.toString()));
+                controlCenter.togglePausePlayAlarm();
+                if(controlCenter.getHouse().isAlarmArmed() && !controlCenter.getHouse().isDoorClosed()){
+                    controlCenter.smartHouse.setIntruderInHouse(true);
+                } else {
+                    controlCenter.smartHouse.setIntruderInHouse(false);
+                }
+                break;
+            case "smart_house/fan":
+                controlCenter.getHouse().setFanOn(Boolean.parseBoolean(var2.toString()));
+                break;
+            case "smart_house/heating_indoor":
+                controlCenter.getHouse().setIndoorHeatOn(Boolean.parseBoolean(var2.toString()));
+                break;
+            case "smart_house/heating_wind":
+                controlCenter.getHouse().setWindHeatOn(Boolean.parseBoolean(var2.toString()));
+                break;
+            case "smart_house/door":
+                controlCenter.getHouse().setDoorClosed(Boolean.parseBoolean(var2.toString()));
+                controlCenter.togglePausePlayAlarm();
+                if(controlCenter.getHouse().isAlarmArmed() && !controlCenter.getHouse().isDoorClosed()){
+                    controlCenter.smartHouse.setIntruderInHouse(true);
+                }
+                break;
+        }
+        System.out.println("reply topic  : " + var1);
+        System.out.println("reply payload: " + var2.toString());
     }
 }
--- src/main/java/se/hkr/SmartHouse.java
+++ src/main/java/se/hkr/SmartHouse.java
@@ -1,7 +1,13 @@
 package se.hkr;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import se.hkr.models.House;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Class SmartHouse is the main simulator class
@@ -13,42 +19,185 @@ public class SmartHouse {
     private static SmartHouse single_instance;
 
     private int additionalConsumption; // adds an abstract value to the el. consumption simulator to simulate
-                                       // elevated consumption when a new device has been turned on
+    // elevated consumption when a new device has been turned on
     private boolean intruderInHouse = false;
     private House house = new House();
 
     MqttClient client = null;
 
+    private SmartHouse(){}
+
     /**
      * houseMock() starts a new thread
      * and runs the readInput() method
      */
-    public void houseMock(){}
+    public void houseMock() {
+        new Thread(() -> {
+            while (true){
+                try {
+                    readInput();
+                } catch (MqttException e) {
+                    System.out.println("Connection to MQTT is lost !!!");
+                }
+            }
+        }).start();
+    }
 
     /**
      * readInput() reads the input and handles
      * the switch statement regulated house behaviour
      */
-    public void readInput(){}
+    public synchronized void readInput() throws MqttException {
+        Scanner in = new Scanner(System.in);
+        int choice = -1;
+        try{
+            choice = Integer.parseInt(in.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Not a number");
+        }
+        switch (choice){
+            case 1:
+                //house.setIndoorLight(!house.isIndoorLight());
+                additionalConsumption = (house.isIndoorLight() ? (additionalConsumption-=10) : (additionalConsumption=10));
+                String inLght = String.valueOf(!house.isIndoorLight());
+                client.publish("smart_house/indoor_light", new MqttMessage(inLght.getBytes()));
+                break;
+            case 2:
+                //house.setOutdoorLight(!house.isOutdoorLight());
+                additionalConsumption = (house.isOutdoorLight() ? (additionalConsumption-=10) : (additionalConsumption+=10));
+                String outLght = String.valueOf(!house.isOutdoorLight());
+                client.publish("smart_house/outdoor_light", new MqttMessage(outLght.getBytes()));
+                break;
+            case 3:
+                house.setWindowOpen(!house.isWindowOpen());
+                break;
+            case 4:
+                String door = String.valueOf(!house.isDoorClosed());
+                ControlCenter.getInstance().clientCheck();
+                client.publish("smart_house/door", new MqttMessage(door.getBytes()));
+                house.setDoorClosed(!house.isDoorClosed());
+                if(house.isAlarmArmed() && !house.isDoorClosed()){
+                    intruderInHouse = true;
+                }
+                ControlCenter.getInstance().togglePausePlayAlarm();
+
+                break;
+            case 5:
+                //house.setAlarmArmed(!house.isAlarmArmed());
+                String alarm = String.valueOf(!house.isAlarmArmed());
+                ControlCenter.getInstance().clientCheck();
+                client.publish("smart_house/alarm", new MqttMessage(alarm.getBytes()));
+                house.isAlarmArmed();
+                if(house.isDoorClosed() && !house.isAlarmArmed())
+                    setIntruderInHouse(false);
+                ControlCenter.getInstance().togglePausePlayAlarm();
+                break;
+            case 0:
+                System.out.println("Quitting the app");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Not a valid input!");
+                break;
+        }
+    }
 
     /**
      * outputsMockData() mocks the data with a Random.class
      * creating an illusion of real temperature and el. consumption data
      */
-    public void outputsMockData(){}
+    public void outputsMockData() {
+        Random r = new Random();
+        AtomicInteger consumption = new AtomicInteger();
+        AtomicInteger indoorTemp = new AtomicInteger();
+        AtomicInteger outdoorTemp = new AtomicInteger();
+        new Thread(() -> {
+            while (true) {
+                consumption.set(r.nextInt(6) + 505 + additionalConsumption);
+                float idoorFloat = (r.nextFloat() + r.nextInt(1) + 20.01f) * 1000.0f;
+                indoorTemp.set((int) idoorFloat);
+                float outdoorFloat = (r.nextFloat() + r.nextInt(2) + 17.01f) * 1000.0f;
+                outdoorTemp.set((int) outdoorFloat);
+
+                printOutputsAndState(consumption, indoorTemp, outdoorTemp);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                printMenu();
+                System.out.println("Enter your command now: ");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 
     /**
      * printMenu() prints a menu with possible choices for commands
      */
-    public void printMenu(){}
+    public void printMenu() {
+
+        System.out.println("\u001B[5m\u001B[4m\u001B[35m" +
+                "########---S-M-A-R-T---H-O-U-S-E---########");
+        System.out.println("1. Toggle INDOOR LIGHT");
+        System.out.println("2. Toggle OUTDOOR LIGHT");
+        System.out.println("3. Toggle WINDOW");
+        System.out.println("4. Toggle DOOR");
+        System.out.println("5. Toggle ALARM");
+        System.out.println();
+        System.out.println("0. Quit");
+        System.out.println("########---------------------------########\u001B[0m");
+        if (intruderInHouse) {
+            System.out.println("\u001B[91m###--I-N-T-R-U-D-E-R---A-L-E-R-T-!-!-!--###\u001B[0m");
+        }
+    }
 
     /**
      * printOutputsAndState() prints constant outputs and state of the house
      */
-    public void printOutputsAndState(){}
+    public void printOutputsAndState(AtomicInteger consumption, AtomicInteger indoorTemp, AtomicInteger outdoorTemp) {
+
+        String consumpt = String.valueOf(consumption);
+        String indoorTmp = String.format("%.02f", (double) (indoorTemp.get() / 1000.0f));
+        String outdoorTmp = String.format("%.02f", (double) (outdoorTemp.get() / 1000.0f));
+
+        System.out.println("\u001B[5m\u001B[4m\u001B[32m########-------O-U-T-P-U-T-S-------########\u001B[0m" + " | " +
+                "\u001B[5m\u001B[4m\u001B[32m####-S-M-A-R-T---H-O-U-S-E---S-T-A-T-E-####\u001B[0m");
+        System.out.println("Indoor temperature:  " + indoorTmp + "                  | " +
+                "Door:          " + (house.isDoorClosed() ? "CLOSED" : "OPEN"));
+        System.out.println("Outdoor temperature: " + outdoorTmp + "                  | " +
+                "Indoor light:  " + (house.isIndoorLight() ? "ON" : "OFF"));
+        System.out.println("Power consumption:   " + consumpt + "                    | " +
+                "Outdoor light: " + (house.isOutdoorLight() ? "ON" : "OFF"));
+        System.out.println("                                            | " +
+                "Window:        " + (house.isWindowOpen() ? "OPEN" : "CLOSED"));
+        System.out.println("                                            | " +
+                "Alarm:         " + (house.isAlarmArmed() ? "ARMED" : "NOT ARMED"));
+        System.out.println("\u001B[5m\u001B[4m\u001B[32m########---------------------------########\u001B[0m" + " | " +
+                "\u001B[5m\u001B[4m\u001B[32m########---------------------------########\u001B[0m");
+
+        if (intruderInHouse) {
+            System.out.println("\u001B[91m###--I-N-T-R-U-D-E-R---A-L-E-R-T-!-!-!--###\u001B[0m");
+        }
+        if (client != null && client.isConnected()) {
+            try {
+                client.publish("smart_house/indoor_temperature", new MqttMessage(indoorTmp.getBytes()));
+                client.publish("smart_house/outdoor_temperature", new MqttMessage(outdoorTmp.getBytes()));
+                client.publish("smart_house/electricity_consumption", new MqttMessage(consumpt.getBytes()));
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     /**
      * getHouse()
+     *
      * @return house
      */
     public House getHouse() {
@@ -57,6 +206,7 @@ public class SmartHouse {
 
     /**
      * getAdditionalConsumption()
+     *
      * @return additionalConsumption
      */
     public int getAdditionalConsumption() {
@@ -65,6 +215,7 @@ public class SmartHouse {
 
     /**
      * setAdditionalConsumption() sets the additionalConsumption to @param
+     *
      * @param additionalConsumption
      */
     public void setAdditionalConsumption(int additionalConsumption) {
@@ -73,6 +224,7 @@ public class SmartHouse {
 
     /**
      * isIntruderInHouse()
+     *
      * @return intruderInHouse
      */
     public boolean isIntruderInHouse() {
@@ -81,6 +233,7 @@ public class SmartHouse {
 
     /**
      * setIntruderInHouse() sets the intruderInHouse to @param
+     *
      * @param intruderInHouse
      */
     public void setIntruderInHouse(boolean intruderInHouse) {
@@ -89,6 +242,7 @@ public class SmartHouse {
 
     /**
      * setClient() sets the MQTT client to @param
+     *
      * @param client
      */
     public void setClient(MqttClient client) {
@@ -97,10 +251,11 @@ public class SmartHouse {
 
     /**
      * getInstance() singleton method
+     *
      * @return SmartHouse instance
      */
-    public static SmartHouse getInstance(){
-        if(single_instance == null){
+    public static SmartHouse getInstance() {
+        if (single_instance == null) {
             single_instance = new SmartHouse();
         }
         return single_instance;
